284b43065dd09bde99eb4d02b8fcf7e52fa4c480
Tomislav Kraljic <63562144+TomislavKraljic@users.noreply.github.com>
2020-11-22 20:44:36 -0500

    used prettier to format docs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/storm-advisory-outline'
+import React from "react";
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/storm-advisory-outline";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <h1> <Icon icon={locationIcon}></Icon>Natural Events Tracker</h1>
-        </header>
-    )}
+  return (
+    <header className="header">
+      <h1>
+        <Icon icon={locationIcon}></Icon>Natural Events Tracker
+      </h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/LocationInfoBox.jsx
+++ src/components/LocationInfoBox.jsx
@@ -4,9 +4,11 @@ const LocationInfoBox = ({ info }) => {
   return (
     <div className="location-info">
       <h3>Location:</h3>
+      
       <p>
         <strong>{info.title}</strong>
       </p>
+
       <p>
         <strong>{info.description}</strong>
       </p>
